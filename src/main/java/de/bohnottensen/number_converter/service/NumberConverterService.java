a7c502a9663441abccac10e3a1ebb5023595ec16
Michael  Bohn <michaelbohn67@gmail.com>
2022-10-24 16:15:59 +0200

    added tests

--- src/main/java/de/bohnottensen/number_converter/service/NumberConverterService.java
+++ src/main/java/de/bohnottensen/number_converter/service/NumberConverterService.java
@@ -1,20 +1,20 @@
 package de.bohnottensen.number_converter.service;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NumberConverterService {
 
-    @PostMapping("/binary/{number}")
-    public String convertIntegerToBinary(@RequestParam int number) {
+    @GetMapping("/binary/{number}")
+    public String convertIntegerToBinary(@PathVariable int number) {
         BinaryNumberService service = new BinaryNumberService();
         return service.convertNumber(number);
     }
 
-    @PostMapping("/roman/{number}")
-    public String convertIntegerToRoman(@RequestParam int number) {
+    @GetMapping("/roman/{number}")
+    public String convertIntegerToRoman(@PathVariable int number) {
         RomanNumberService service = new RomanNumberService();
         return service.convertNumber(number);
     }
--- src/test/java/de/bohnottensen/number_converter/NumberConverterApplicationTests.java
+++ src/test/java/de/bohnottensen/number_converter/NumberConverterApplicationTests.java
@@ -1,13 +1,26 @@
 package de.bohnottensen.number_converter;
 
+import de.bohnottensen.number_converter.service.NumberConverterService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest
 class NumberConverterApplicationTests {
 
+    @Autowired
+    private NumberConverterService service;
+
+    @Autowired
+    private NumberConverterApplication numberConverterApplication;
+
     @Test
     void contextLoads() {
+        assertThat(service, notNullValue());
+        assertThat(numberConverterApplication, notNullValue());
     }
 
 }
